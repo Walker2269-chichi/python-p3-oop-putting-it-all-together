f1a5ea1a474c4eb451ee48c05b413120792709db
jeremiahlangat <Langatjeremiah22@gmail.com>
2025-02-03 11:02:47 +0300

    OOP

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
-
 class Book:
-    pass
+    def __init__(self, title, author, genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.is_checked_out = False
+
+    def check_out(self):
+        if not self.is_checked_out:
+            self.is_checked_out = True
+        else:
+            raise ValueError("Book is already checked out")
+
+    def check_in(self):
+        if self.is_checked_out:
+            self.is_checked_out = False
+        else:
+            raise ValueError("Book is not checked out")
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, size, color, brand):
+        self.size = size
+        self.color = color
+        self.brand = brand
+
+    def __str__(self):
+        return f"{self.brand} {self.color} Shoe (Size {self.size})"
